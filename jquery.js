a7e75ec104484c1fc6bfd4c6c1dfce595450c63a
Kara Wilson <63965607+karawilson1@users.noreply.github.com>
2021-01-18 22:21:38 -0500

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,1 +1,9 @@
+$("form").on("keypress", function () {
+  $("value").html("Your request has been submitted.")
+  
+  )}
 
+$("p").on("click", function () {
+  $(this).css("background-color", "#CC32E5")
+  
+})
