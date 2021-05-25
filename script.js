8a9efaaf97f20d3679f54f5eeaa6e1bff973190b
karawilson1 <karaewilson16@gmail.com>
2021-05-25 09:57:21 -0400

    js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-$("#interaction").hide();
-setInterval(function(){
-	$("#interaction").show();
-},4000);
 
 window.onload=function showInteraction()  //executes when the page finishes loading
 
 {
-	setTimeout(func1, 4000);  //sets a timer which calls function func1 after 2,000 milliseconds = 2 secs.
+	setTimeout(func1, 4000);  //sets a timer which calls function func1 after 4,000 milliseconds = 4 secs.
 };
 
 function showInteraction()
 {
-	document.getElementById("#interaction").className="show";
+	document.getElementById("#interaction").className=".show";
 }
