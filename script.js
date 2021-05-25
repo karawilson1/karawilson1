25c8c62ef6f4bbc3e04f52c23037026ac08fe526
karawilson1 <karaewilson16@gmail.com>
2021-05-25 12:05:20 -0400

    js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ setTimeout(function() {
 window.addEventListener('load', ()=>{
 
 	resize(); // Resizes the canvas once the window loads
+  document.addEventListener('mousedown', startPainting);
+  document.addEventListener('mouseup', stopPainting);
   document.addEventListener('mousemove', sketch);
 	window.addEventListener('resize', resize);
 });
@@ -40,7 +42,13 @@ coord.y = event.clientY - canvas.offsetTop;
 
 // The following functions toggle the flag to start
 // and stop drawing
-
+function startPainting(event){
+  paint = true;
+  getPosition(event);
+}
+function stopPainting(){
+  paint = false;
+}
 
 function sketch(event) {
   if (!paint) return;
