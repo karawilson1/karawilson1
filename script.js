a0e41afeca176e843ee40b77ff1ec46f2a4a6ece
karawilson1 <karaewilson16@gmail.com>
2021-05-25 10:12:06 -0400

    js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-
-window.onload=function showInteraction()  //executes when the page finishes loading
-
-{
-	setTimeout(showInteraction, 4000);  //sets a timer which calls function func1 after 4,000 milliseconds = 4 secs.
-};
-
-function showInteraction()
-{
-	document.getElementById("#interaction").addClass=".show";
-}
+$(function showInteraction() {
+  $(window).load(function() {
+    document.getElementById("#interaction").addClass=".show";
+  })
+  });
