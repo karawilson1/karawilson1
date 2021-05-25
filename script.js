f817f256213dfaca44d3821fb5058c9288a7cb16
karawilson1 <karaewilson16@gmail.com>
2021-05-25 09:35:46 -0400

    interaction

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ $("#interaction").hide();
 setInterval(function(){
 	$("#interaction").show();
 },4000);
+
+window.onload=function showInteraction()  //executes when the page finishes loading
+
+{
+	setTimeout(func1, 4000);  //sets a timer which calls function func1 after 2,000 milliseconds = 2 secs.
+};
+
+function showInteraction()
+{
+	document.getElementById("#interaction").className="show";
+}
